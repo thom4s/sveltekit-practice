bbbfda83ddcb9508c90d375b229976db1a4e1046
thomas <hello@thomasflorentin.net>
2022-10-22 15:28:59 +0200

    register: Polish
    not totaly ok on msg….

--- src/routes/register/+page.server.js
+++ src/routes/register/+page.server.js
@@ -5,21 +5,28 @@ const api_url = `https://pactice-strapi-cms.herokuapp.com/api`;
 export const actions = {
     register: async ({request}) => {
         
+        // GET DATAS FROM FORM
         const formData = await request.formData();
         const username = formData.get('username');
         const email = formData.get('email');
         const confirmedEmail = formData.get('confirmedEmail');
         const password = formData.get('password');
 
+
+        // MY VALIDATIONS
         if( email !== confirmedEmail ) {
             return {
                 data: {
                     error: true,
                     message: "email are not matching...",
+                    username, 
+                    email
                 }
             }
         }
 
+
+        // REGISTER FUNC
         const registerUser = async () => {
 
             const url = `${api_url}/auth/local/register`;
@@ -42,26 +49,34 @@ export const actions = {
                 .then(res => res.json())
                 .then(response => {
                     console.log('response:', response )
+                    console.log('response.error:', response.error )
 
+                    // There is an error
                     if( response.error) {
                         return {
                             error: true,
                             name: response.error.name,
                             message: response.error.message,
-                            details: response.error.details
+                            details: response.error.details,
+                            username, 
+                            email
                         }
                     }
 
+                    console.log('everything allright')
+                    // evrything's alright
                     return {
                         error: false,
-                        message: response.message
+                        message: response.message,
+                        username, 
+                        email
                     }
                 
                     // console.log('User profile', response.user);
                     // console.log('User token', response.jwt);
                 })
                 .catch(error => {
-                    console.error("error:", error);
+                    console.error("error:", error.json());
 
                     return {
                         error,
@@ -69,8 +84,8 @@ export const actions = {
                     }
                 });
         }
-        
 
+        // RETURN WHAT FORM RETURN
         return {
             data: await registerUser(),
             message: "hey"
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { enhance, applyAction } from '$app/forms';
     import Alert from '$lib/components/Alert.svelte';
 
     export let data;
@@ -20,15 +21,31 @@
         <h1>Register</h1>
     </div>
 
-    <form method="POST" action="?/register" >
+    <form method="POST" action="?/register" 
+        use:enhance={ ({form}) => {
+            // Before form submission to server     
+
+            return async ({result, update}) => {
+                // After form submission to server
+                if( result.type === 'success') {
+                    form.reset();
+                }
+                if( result.type === 'invalid') {
+                    await applyAction(result);
+                }
+
+                update();
+            }
+        }
+    }>
         <p>
-            <input type="text" placeholder="username" name="username" value="{form?.username ?? ''}">
+            <input type="text" placeholder="username" name="username" value="{form?.data.username ?? ''}">
         </p>
         <p>
-            <input type="email" placeholder="email" name="email" value="{form?.email ?? ''}">
+            <input type="email" placeholder="email" name="email" value="{form?.data.email ?? ''}">
         </p>
         <p>
-            <input type="email" placeholder="confirmer votre email" name="confirmedEmail" value="{form?.confirmedEmail ?? ''}">
+            <input type="email" placeholder="confirmer votre email" name="confirmedEmail" value="">
         </p>
         <p>
             <input type="password" placeholder="mdp" name="password" value="{form?.password ?? ''}">
